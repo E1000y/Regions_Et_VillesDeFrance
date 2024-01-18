89f3f832efcc6bd37447d2e546bb03f871395125
Emilie Paniagua <emilie.paniagua@gmail.com>
2024-01-18 09:41:20 +0100

    first commit

---
+++ src/test/java/fr/EmiliePaniagua/poec/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.EmiliePaniagua.poec.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
